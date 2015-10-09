1841cbbfc702cb18b76163a8d0901cfd4b71a8d3
fsamad <fabdelsa@hawk.iit.edu>
2015-10-09 15:41:29 -0500

    // Launch setup code

---
+++ launch.sh
@@ -0,0 +1,2 @@
+aws ec2 run-instances --image-id ami-d05e75b8 --count 2 --instance-type t2.micro --key-name itmo544-fall2015 --security-group-ids sg-6d6a7c0a --subnet-id subnet-6bb1a51c --associate-public-ip-address
+
