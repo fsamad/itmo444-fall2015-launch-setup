40a17edf4b675f7ced9880aba8967cd8dc28ca01
fabdelsa <fabdelsa@hawk.iit.edu>
2015-10-29 01:10:10 -0500

    changed configuration name

--- launch.sh
+++ launch.sh
@@ -25,8 +25,8 @@ aws elb register-instances-with-load-balancer --load-balancer-name $2 --instance
 aws elb configure-health-check --load-balancer-name $2 --health-check Target=HTTP:80/index.html,Interval=30,UnhealthyThreshold=2, HealthyThreshold=2,Timeout=3
 
 #launch configuration
-aws autoscaling create-launch-configuration --launch-configuration-name itmo-launch-conf --image-id ami-d05e75b8 --key-name itmo544-fall2015 --security-groups sg-6d6a7c0a --instance-type t2.micro --user-data file://install-env.sh --iam-instance-profile phpDeveloperRole
+aws autoscaling create-launch-configuration --launch-configuration-name itmo-fa-launch-conf --image-id ami-d05e75b8 --key-name itmo544-fall2015 --security-groups sg-6d6a7c0a --instance-type t2.micro --user-data file://install-env.sh --iam-instance-profile phpDeveloperRole
 
 #creating the autoscaling group
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name itmo-autoscaling-group --launch-configuration-name itmo-launch-conf --load-balancer-names $2 --health-check-type ELB --min-size 3 --max-size 6 desired-capacity 3 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier subnet-6bb1a51c 
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name itmo-autoscaling-group --launch-configuration-name itmo-fa-launch-conf --load-balancer-names $2 --health-check-type ELB --min-size 3 --max-size 6 desired-capacity 3 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier subnet-6bb1a51c 
 
