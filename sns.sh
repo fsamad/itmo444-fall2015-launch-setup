679dd715be894806c2129322f47de61e56bb06e8
fabdelsa <fabdelsa@hawk.iit.edu>
2015-11-21 21:33:40 -0600

    added code

--- sns.sh
+++ sns.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-aws sns create-topic --name mp2
+ARN=('aws sns create-topic --name mp2')
+echo "This is the ARN: $ARN"
+
+aws sns --set-topic-attributes --topic-arn $ARN --attribute-name DisplayName --attribute-value mp2
+
+aws sns subscribe --topic-arn $ARN --protocol sms --notification-endpoint 13128334094
